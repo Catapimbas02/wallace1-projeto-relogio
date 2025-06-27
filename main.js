6609d1ca2c55a2584755ce41e4e3ef61c4be2f1b
Catapimbas02 <162309912+Catapimbas02@users.noreply.github.com>
2025-06-27 09:42:42 -0300

    krl

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-const  botoes = document.querySelectorAll("botao");
-console (botoes)
+const botoes = document.querySelectorAll(".botoes");
+const textos = document.querySelectorAll(".abas-conteudo");
+const contadores = document.querySelectorAll(".contador");
+console.log(botoes)
 
-for(i=0;i<botoes.length;i++){
+for(let i=0; i< botoes.length; i++){
     botoes[i].onclick = function(){
-        for(j=0; j<botoes.length; j++){
-            botoes[j].classList.remove(ativo);
+        
+        for (let j=0; j<botoes.length; j++){
+            botoes[j].remove.add("ativo");
+            textos[j].remove.add("ativo");
         }
-        botoes[i].classList.add(ativo);
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
